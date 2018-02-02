foxallensimers/spring-boot

commit 8af02ce05bbd395d28a3d4af87e6b0d02409606e

Allow custom AbstractHealthIndicator warning logs
Update `AbstractHealthIndicator` so that the warning message can be
customized. Also updated our existing indicators with better messages.

Fixes gh-11880

spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/jdbc/DataSourceHealthIndicator.java
@@ -64,6 +64,7 @@ public class DataSourceHealthIndicator extends AbstractHealthIndicator
 	 * Create a new {@link DataSourceHealthIndicator} instance.
 	 */
 	public DataSourceHealthIndicator() {
+		this(null, null);
 	}
 
 	/**
@@ -82,6 +83,7 @@ public class DataSourceHealthIndicator extends AbstractHealthIndicator
 	 * @param query the validation query to use (can be {@code null})
 	 */
 	public DataSourceHealthIndicator(DataSource dataSource, String query) {
+		super("DataSource health check failed");
 		this.dataSource = dataSource;
 		this.query = query;
 		this.jdbcTemplate = new JdbcTemplate(dataSource);

spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/amqp/RabbitHealthIndicator.java
@@ -34,6 +34,7 @@ public class RabbitHealthIndicator extends AbstractHealthIndicator {
 	private final RabbitTemplate rabbitTemplate;
 
 	public RabbitHealthIndicator(RabbitTemplate rabbitTemplate) {
+		super("Rabbit health check failed");
 		Assert.notNull(rabbitTemplate, "RabbitTemplate must not be null");
 		this.rabbitTemplate = rabbitTemplate;
 	}

spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/cassandra/CassandraHealthIndicator.java
@@ -37,6 +37,10 @@ public class CassandraHealthIndicator extends AbstractHealthIndicator {
 
 	private CassandraOperations cassandraOperations;
 
+	public CassandraHealthIndicator() {
+		super("Cassandra health check failed");
+	}
+
 	/**
 	 * Create a new {@link CassandraHealthIndicator} instance.
 	 * @param cassandraOperations the Cassandra operations

spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/couchbase/CouchbaseHealthIndicator.java
@@ -37,6 +37,10 @@ public class CouchbaseHealthIndicator extends AbstractHealthIndicator {
 
 	private CouchbaseOperations operations;
 
+	public CouchbaseHealthIndicator() {
+		super("Couchbase health check failed");
+	}
+
 	public CouchbaseHealthIndicator(CouchbaseOperations couchbaseOperations) {
 		Assert.notNull(couchbaseOperations, "CouchbaseOperations must not be null");
 		this.operations = couchbaseOperations;

spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/elasticsearch/ElasticsearchHealthIndicator.java
@@ -65,6 +65,7 @@ public class ElasticsearchHealthIndicator extends AbstractHealthIndicator {
 	 */
 	public ElasticsearchHealthIndicator(Client client, long responseTimeout,
 			String... indices) {
+		super("Elasticsearch health check failed");
 		this.client = client;
 		this.responseTimeout = responseTimeout;
 		this.indices = indices;

spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/elasticsearch/ElasticsearchJestHealthIndicator.java
@@ -40,6 +40,7 @@ public class ElasticsearchJestHealthIndicator extends AbstractHealthIndicator {
 	private final JsonParser jsonParser = new JsonParser();
 
 	public ElasticsearchJestHealthIndicator(JestClient jestClient) {
+		super("Elasticsearch health check failed");
 		this.jestClient = jestClient;
 	}
 

spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/health/AbstractHealthIndicator.java
@@ -16,10 +16,14 @@
 
 package org.springframework.boot.actuate.health;
 
+import java.util.function.Function;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.springframework.boot.actuate.health.Health.Builder;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 /**
  * Base {@link HealthIndicator} implementations that encapsulates creation of
@@ -34,7 +38,44 @@ import org.springframework.boot.actuate.health.Health.Builder;
  */
 public abstract class AbstractHealthIndicator implements HealthIndicator {
 
+	private static final String NO_MESSAGE = null;
+
+	private static final String DEFAULT_MESSAGE = "Health check failed";
+
 	private final Log logger = LogFactory.getLog(getClass());
+
+	private final Function<Exception, String> healthCheckFailedMessage;
+
+	/**
+	 * Create a new {@link AbstractHealthIndicator} instance with a default
+	 * {@code healthCheckFailedMessage}.
+	 */
+	public AbstractHealthIndicator() {
+		this(NO_MESSAGE);
+	}
+
+	/**
+	 * Create a new {@link AbstractHealthIndicator} instance with a specific message to
+	 * log when the health check fails.
+	 * @param healthCheckFailedMessage the message to log on health check failure
+	 * @since 2.0.0
+	 */
+	protected AbstractHealthIndicator(String healthCheckFailedMessage) {
+		this.healthCheckFailedMessage = (ex) -> healthCheckFailedMessage;
+	}
+
+	/**
+	 * Create a new {@link AbstractHealthIndicator} instance with a specific message to
+	 * log when the health check fails.
+	 * @param healthCheckFailedMessage the message to log on health check failure
+	 * @since 2.0.0
+	 */
+	protected AbstractHealthIndicator(
+			Function<Exception, String> healthCheckFailedMessage) {
+		Assert.notNull(healthCheckFailedMessage,
+				"HealthCheckFailedMessage must not be null");
+		this.healthCheckFailedMessage = healthCheckFailedMessage;
+	}
 
 	@Override
 	public final Health health() {
@@ -43,7 +84,11 @@ public abstract class AbstractHealthIndicator implements HealthIndicator {
 			doHealthCheck(builder);
 		}
 		catch (Exception ex) {
-			this.logger.warn("Health check failed", ex);
+			if (this.logger.isWarnEnabled()) {
+				String message = this.healthCheckFailedMessage.apply(ex);
+				this.logger.warn(StringUtils.hasText(message) ? message : DEFAULT_MESSAGE,
+						ex);
+			}
 			builder.down(ex);
 		}
 		return builder.build();

spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/health/ApplicationHealthIndicator.java
@@ -25,6 +25,10 @@ package org.springframework.boot.actuate.health;
  */
 public class ApplicationHealthIndicator extends AbstractHealthIndicator {
 
+	public ApplicationHealthIndicator() {
+		super("Application health check failed");
+	}
+
 	@Override
 	protected void doHealthCheck(Health.Builder builder) throws Exception {
 		builder.up();

commit 719fb96530adbfbeb15a7cce5a4ef559ca630a57

Fix DataSourceHealthIndicator test failure
Fix failure introduced in commit 8af02ce05b.

See gh-11880

spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/jdbc/DataSourceHealthIndicator.java
@@ -86,7 +86,7 @@ public class DataSourceHealthIndicator extends AbstractHealthIndicator
 		super("DataSource health check failed");
 		this.dataSource = dataSource;
 		this.query = query;
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.jdbcTemplate = (dataSource == null ? null : new JdbcTemplate(dataSource));
 	}
 
 	@Override